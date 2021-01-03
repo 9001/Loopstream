08c656b65ab307e1a6e2a06e46ce744efe2400a5
ed <s@ocv.me>
2021-01-03 03:08:30 +0100

    gdi

---
+++ Loopstream/UC_HintedLabel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace Loopstream
+{
+    public partial class HintedLabel : Label
+    {
+        public TextRenderingHint Hinting { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TextRenderingHint = Hinting;
+            base.OnPaint(e);
+        }
+    }
+}
